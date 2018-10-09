452e2e57d5349ec83f3d6040a7eabda678d519ca
Frazew <frazewmc@gmail.com>
2018-10-09 22:23:11 +0200

    Test webhook en json ase enter the commit message for your changes. Lines starting

--- github_push.php
+++ github_push.php
@@ -1,5 +1,20 @@
 <?php
 $SECRET = "DpR0RyYl6abP32WSW6Se";
+
+if (isset(getallheaders()["X-Hub-Signature"])) {
+	$signature = getallheaders()["X-Hub-Signature"];
+	$body = file_get_contents('php://input');
+	$hmac = hash_hmac("sha1", $body, $SECRET);
+	if (hash_equals($signature, $hmac)) {
+		echo "ok";
+	} else {
+		http_response_code(403);
+		die("Invalid GitHub authorisation header");
+	}
+} else {
+	http_response_code(403);
+	die("Missing GitHub authorisation header");
+}
 file_put_contents(json_encode(headers()), "headers");
 file_put_contents(json_encode($_POST), "post");
 ?>
